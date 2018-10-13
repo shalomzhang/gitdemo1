5acaaa48d526402a8c57dab8cb9fb05e0ef55cc6
zhangxiaoliang <zhang_xiaoliang@foxmail.com>
2018-10-13 10:45:35 +0800

    11111

--- src/main/java/FirstTest.java
+++ src/main/java/FirstTest.java
@@ -1,5 +1,5 @@
 public class FirstTest {
     public static void main(String[] args) {
-        System.out.println("firsttest第三次修改");
+        System.out.println("firsttest第a次修改");
     }
 }
--- src/main/java/SecondTest.java
+++ src/main/java/SecondTest.java
@@ -1,5 +1,5 @@
 public class SecondTest {
     public static void main(String[] args) {
-        System.out.println("第二个文件第一次修改");
+        System.out.println("第二个文件第a次修改");
     }
 }

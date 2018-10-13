baa8a28140ba24d1706e599f00b239dec4073fe2
zhangxiaoliang <zhang_xiaoliang@foxmail.com>
2018-10-13 22:24:23 +0800

    4branch

--- src/main/java/FirstTest.java
+++ src/main/java/FirstTest.java
@@ -3,6 +3,6 @@ public class FirstTest {
         System.out.println("firsttestÊÇ·ñÄÜµ½new2");
     }
     public void show(){
-        System.out.println("branch1");
+        System.out.println("branch14444");
     }
 }

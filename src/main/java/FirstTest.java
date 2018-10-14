d48045c6d647c1b655c148bce16f3810e595a07f
shalomzhang <zhang_xiaoliang@foxmail.com>
2018-10-14 10:08:45 +0800

    Update FirstTest.java

--- src/main/java/FirstTest.java
+++ src/main/java/FirstTest.java
@@ -1,6 +1,6 @@
 public class FirstTest {
     public static void main(String[] args) {
-        System.out.println("firsttestmasteredit");
+        System.out.println("firsttestmasteredit1");
     }
     public void show(){
         System.out.println("branch555551new666");
